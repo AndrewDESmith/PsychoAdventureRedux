0c0de150acce127d23a4e2775a18eca7b1a4ef05
Andrew Bolt <andrewebolt@gmail.com>
2014-07-23 17:55:25 -0400

    Bolt - Remember Me functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
   has_many :user_scenes
 
-  validates :password, length: { minimum: 3 }
+  validates :password, length: { minimum: 8 }
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
 
