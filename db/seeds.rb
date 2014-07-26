ca97cbe8e0da24f2aedf4c046f6d45f2fc570148
Andrew Bolt <andrewebolt@gmail.com>
2014-07-26 15:38:31 -0400

    Made some fake seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,12 @@
 
 Scene.create(location: "Planet Earth", story_body: "Test seed story_body entry.", parent_scene_id: 1, time: "12:00am", date: "July 1, 19XX", relevant_clue: false)
 
-Scene.create(location: "Planet Mars", story_body: "Test seed story_body entry numero dos.", parent_scene_id: 2, time: "8:00am", date: "July 1, 20XX", relevant_clue: true)
-
+Scene.create(location: "Planet Mars", story_body: "Test seed story_body entry numero dos.", parent_scene_id: 1, time: "8:00am", date: "July 1, 20XX", relevant_clue: true)
+Scene.create(location: "Planet Mercury", story_body: "Test seed story_body entry numero tres.", parent_scene_id: 1, time: "9:00am", date: "July 2, 20XX", relevant_clue: false)
+Scene.create(location: "Planet Venus", story_body: "Test seed story_body entry numero quatro.", parent_scene_id: 1, time: "10:00am", date: "July 3, 20XX", relevant_clue: false)
+Scene.create(location: "Planet Jupiter", story_body: "Test seed story_body entry numero 5.", parent_scene_id: 2, time: "8:00am", date: "July 1, 20XX", relevant_clue: true)
+Scene.create(location: "Planet Saturn", story_body: "Test seed story_body entry numero 6.", parent_scene_id: 2, time: "8:00am", date: "July 1, 20XX", relevant_clue: true)
+Scene.create(location: "Planet Neptune", story_body: "Test seed story_body entry numero 7.", parent_scene_id: 2, time: "8:00am", date: "July 1, 20XX", relevant_clue: true)
 # User.create(character_name: "AndrewS", email: "andrews@email.com", crypted_password: "password", salt: nil, remember_me: nil, remember_me_token_expires_at: nil)
 
 # User.create(character_name: "AndrewB", email: "andrewb@email.com", crypted_password: "password")
