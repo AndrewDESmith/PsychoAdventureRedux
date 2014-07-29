363d797be843da4298b57da87c7ffe8b78ac83c9
patszkarlat <punkgeek@mac.com>
2014-07-29 15:14:07 -0400

    added story file

--- db/migrate/20140728183154_remove_parent_scene_id_from_scenes.rb
+++
@@ -1,5 +0,0 @@
-class RemoveParentSceneIdFromScenes < ActiveRecord::Migration
-  def change
-    remove_column :scenes, :parent_scene_id, :integer
-  end
-end
--- db/migrate/20140728183543_add_choice_text_to_scenes.rb
+++
@@ -1,5 +0,0 @@
-class AddChoiceTextToScenes < ActiveRecord::Migration
-  def change
-    add_column :scenes, :choice_text, :text
-  end
-end
