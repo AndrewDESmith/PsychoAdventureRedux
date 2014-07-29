b0fc882e8bd2ad7709947d1f4e7b2f0a292735ec
Andrew Smith <andrew.d.e.smith@gmail.com>
2014-07-29 16:59:21 -0400

    Got Foundation working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,4 @@
-a// This is a manifest file that'll be compiled into application.js, which will include all the files
+// This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
@@ -12,7 +12,7 @@ a// This is a manifest file that'll be compiled into application.js, which will 
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require foundation
 //= require_tree .
 $(document).foundation();
+$(function(){ $(document).foundation(); });
