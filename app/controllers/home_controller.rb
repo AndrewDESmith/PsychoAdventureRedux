7337c389916b0393e5412f227439dc683deab96c
Andrew Smith <andrew.d.e.smith@gmail.com>
2014-07-30 18:34:02 -0400

    Home page now dynamically links to the first show page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 	skip_before_filter :require_login
-	
+
 	def home
+		@scenes = Scene.all
 	end
 end
--- db/migrate/20140728183251_add_primary_scene_id_to_scenes.rb
+++
@@ -1,5 +0,0 @@
-class AddPrimarySceneIdToScenes < ActiveRecord::Migration
-  def change
-    add_column :scenes, :primary_scene_id, :integer
-  end
-end
