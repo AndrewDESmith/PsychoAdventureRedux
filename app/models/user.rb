d92832621bbeed5b96dd6e7b19d2210f6689fc87
Andrew Bolt <andrewebolt@gmail.com>
2014-07-30 17:37:59 -0400

    Adding the Notebook on scenes show pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   has_many :user_scenes
+  has_many :scenes, :through => :user_scenes
 
   validates :password, length: { minimum: 8 }
   validates :password, confirmation: true
