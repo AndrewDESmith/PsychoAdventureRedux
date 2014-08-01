04339a5d0552d0312f13efd1905c5847acadd2fc
Andrew Bolt <andrewebolt@gmail.com>
2014-08-01 09:01:39 -0400

    Faded buttons on hover

--- app/assets/javascripts/homepage_fadein.js
+++ app/assets/javascripts/homepage_fadein.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-$('h1.headline, div#nav, div.begin').hide().fadeIn(1300);
+	$('h1.headline, div#nav, div.begin').hide().fadeIn(1300);
+	$('div.botton').mouseenter(function(){
+		$$('div.botton').fadeTo('fast', 0.1)
+	});
+	$('div.botton').mouseleave(function(){
+		$('div.botton').fadeTo('fast', 1)
+	});
 });	
